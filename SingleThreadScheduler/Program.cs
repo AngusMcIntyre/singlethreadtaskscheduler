3fd4a548d418360117324cd2bf3aa9ecebd2c192
Angus McIntyre <gusdor@gmail.com>
2017-11-22 23:35:33 +0000

    Add project files.

---
+++ SingleThreadScheduler/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingleThreadScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SingleThreadScheduler scheduler = new SingleThreadScheduler())
+            {
+                var tasks = GenerateTaskBatch(scheduler);
+
+                scheduler.Dispose();
+
+                tasks = GenerateTaskBatch(scheduler);
+
+                Task.WaitAll(tasks);
+
+                tasks = GenerateTaskBatch(TaskScheduler.Default);
+
+                Task.WaitAll(tasks);
+
+                Console.WriteLine("Done. Press ENTER to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static Task[] GenerateTaskBatch(TaskScheduler scheduler)
+        {
+            Console.WriteLine("Press ENTER to execute tasks on {0}.", scheduler.GetType().Name);
+            Console.ReadLine();
+
+            Task[] tasks = new Task[10];
+
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                int actionNumber = i;
+                tasks[i] = RunTaskOnScheduler(() =>
+                {
+                    Console.WriteLine($"TID: {Thread.CurrentThread.ManagedThreadId}, Executing action {actionNumber + 1}");
+                }, 
+                scheduler);
+            }
+
+            return tasks;
+        }
+
+        private static Task RunTaskOnScheduler(Action action, TaskScheduler scheduler)
+        {
+            TaskFactory factory = new TaskFactory(
+                CancellationToken.None, 
+                TaskCreationOptions.DenyChildAttach,
+                TaskContinuationOptions.None, 
+                scheduler);
+
+            return factory.StartNew(action);
+        }
+    }
+}
---
+++ SingleThreadScheduler/SingleThreadScheduler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingleThreadScheduler
+{
+    /// <summary>
+    /// Implementation of <see cref="TaskScheduler"/> that runs tasks on a single, dedicated thread.
+    /// </summary>
+    internal class SingleThreadScheduler : TaskScheduler, IDisposable
+    {
+        private BlockingCollection<Task> scheduledTasks = new BlockingCollection<Task>();
+        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        public SingleThreadScheduler()
+        {
+            this.StartWork();
+        }
+
+        private void StartWork()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                for (; ; )
+                {
+                    if (scheduledTasks.IsAddingCompleted)
+                    {
+                        break;
+                    }
+
+                    var task = scheduledTasks.Take(this.cancellationTokenSource.Token);
+                    base.TryExecuteTask(task);
+                }
+            },
+            this.cancellationTokenSource.Token,
+            TaskCreationOptions.LongRunning,
+            TaskScheduler.Default);
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return scheduledTasks.AsEnumerable();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            this.scheduledTasks.Add(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            // Tasks cannot be executed inline.
+            return false;
+        }
+
+        public void Dispose()
+        {
+            scheduledTasks.CompleteAdding();
+            cancellationTokenSource.Cancel();
+            //((IDisposable)this.cancellationTokenSource).Dispose();
+            //((IDisposable)this.scheduledTasks).Dispose();
+        }
+    }
+}
